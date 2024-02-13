5ee3c77c4b7b9fc66f87c3d53098303e7c92c583
gioqareli <kareligiorgi121@gmail.com>
2024-02-13 20:55:58 +0400

    first commit

---
+++ Day 01/homework/lesson1.py
@@ -0,0 +1,14 @@
+name=" me var giorgi"
+
+surname="qareli"
+
+age="14 wlis"
+
+height="1.55 simagleshi"
+
+life="vcxovrob gorshi"
+
+print(name+ "  " + surname + "  "+  age+"  " + height+ "  " + life )
+
+
+
