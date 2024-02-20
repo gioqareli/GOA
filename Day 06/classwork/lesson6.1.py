1f0ba2686e152383d5f59458c2930a223fd97cc8
gioqareli <kareligiorgi121@gmail.com>
2024-02-20 20:48:45 +0400

    commit

---
+++ Day 06/classwork/lesson6.1.py
@@ -0,0 +1,5 @@
+user_name = input ("what is your name?:  ")
+user_age = input ("what is your age?:  ")
+user_surname =input  ("what is your surname?:  ")
+
+print( " your  name is " +  user_name  +  " your surname is  "  +  user_surname  +  " and you are "  +  user_age  )
---
+++ Day 06/homework/leeson6.2.py
@@ -0,0 +1,5 @@
+user_age = input(" how old are you?: ")
+user_name = input("whats your name?: ")
+user_surname = input("whats your surname?: ")
+user_age = user_age + 20
+print("your name is " + user_name + "your surname is " + user_surname + "and you are " + user_age)
