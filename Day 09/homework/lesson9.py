a65ad476c68689ff24cc33a62a348a8106263dfc
gioqareli <kareligiorgi121@gmail.com>
2024-03-01 19:43:40 +0400

    commit

---
+++ Day 09/homework/lesson9.py
@@ -0,0 +1,5 @@
+#მომხმარებელს შემოატანინეთ ორი რიცხვი, მისი ასაკი და მშობლის ასაკი, შემდეგ კი შეადარეთ ისინი ერთმანეთს
+
+my_age = input("how old are you ?:  ")
+parent_age = input("what is your mom age ?: ")
+print(my_age < parent_age)
