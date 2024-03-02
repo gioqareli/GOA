1ec9d07aebe505ef80d6d8d5e6ce85ce3cb65720
gioqareli <kareligiorgi121@gmail.com>
2024-03-02 09:13:04 +0400

    commit

--- Day 09/homework/lesson9.py
+++ Day 09/homework/lesson9.py
@@ -2,4 +2,6 @@
 
 my_age = input("how old are you ?:  ")
 parent_age = input("what is your mom age ?: ")
-print(my_age < parent_age)
+bulion = my_age < parent_age
+print(bulion)
+print(type(bulion))
