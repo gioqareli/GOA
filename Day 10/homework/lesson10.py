fec5a77a1353e8b081f4c558b1c5721b0d7621e7
gioqareli <kareligiorgi121@gmail.com>
2024-03-05 17:22:25 +0400

    commit

---
+++ Day 10/homework/lesson10.py
@@ -0,0 +1,24 @@
+a =  input(" do you like goa:?  ")
+b =  input(" do you like novator:?  ") 
+ab = a > b 
+print(ab)
+print(a and b)
+
+a = input("do you like novator:?  ")
+b = input("do you like goa:?  ")
+print(a or b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
