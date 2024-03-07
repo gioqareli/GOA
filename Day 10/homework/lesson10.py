ecebe1f75b152589892ccce5b81e06e920067785
gioqareli <kareligiorgi121@gmail.com>
2024-03-07 23:23:27 +0400

    commit

--- Day 10/homework/lesson10.py
+++ Day 10/homework/lesson10.py
@@ -1,12 +1,11 @@
+
+ #მომხმარებელს შემოატანინეთ ორი ბულიან ტიპის მნიშვნელობა(true, false), შემდეგ კი დაბეჭდეთ ერკანზე ეს მნიშვნელობები      შედარებული ერთმანეთზე, ამისთვის გამოიყენეთ ლოგიკური ოპერატორები (and და or)
 a =  input(" do you like goa:?  ")
 b =  input(" do you like novator:?  ") 
-ab = a > b 
-print(ab)
 print(a and b)
 
-a = input("do you like novator:?  ")
-b = input("do you like goa:?  ")
 print(a or b)
+
 
 
 
---
+++ Day 11/homework/lesson11.py
@@ -0,0 +1,8 @@
+mom_age = int(input("what is your mom age :? ") )
+dad_age = int(input("what is your dad age :?") )
+
+if mom_age > dad_age:
+    print("ამ შემთხვევაში მამა დიდია დედაზე ასაკით")
+
+if dad_age > mom_age:
+    print("ამ შემთხვევაში დედა დიდია მამაზე ასაკით")
